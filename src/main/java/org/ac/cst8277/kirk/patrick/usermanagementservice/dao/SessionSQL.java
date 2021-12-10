bafbf3c1be1483cd074fc5bc5ad7d66e56fa78ca
Patrick Kirk <kirk0216@algonquinlive.com>
2021-12-10 15:34:19 -0500

    Corrected SQL syntax error in session delete SQL.

--- src/main/java/org/ac/cst8277/kirk/patrick/usermanagementservice/dao/SessionSQL.java
+++ src/main/java/org/ac/cst8277/kirk/patrick/usermanagementservice/dao/SessionSQL.java
@@ -4,7 +4,7 @@ public final class SessionSQL {
     public static final String INSERT =
             "INSERT INTO ums.session (user_id, token) VALUES (?, ?);";
     public static final String DELETE =
-            "DELETE FROM ums.session WHERE user_id = ?, token = ?;";
+            "DELETE FROM ums.session WHERE user_id = ? AND token = ?;";
     public static final String GET_BY_TOKEN =
             "SELECT * FROM ums.session WHERE token = ?;";
     public static final String GET_BY_USER_ID =
